97eb57b495e20410785252de51295af614e20617
hienthu67 <53996734+hienthu67@users.noreply.github.com>
2021-08-13 06:09:39 +0700

    của hàng bán điện thoại

---
+++ assets/js/home.js
@@ -0,0 +1,541 @@
+const data = [
+    {
+        id: 0,
+        img: 'assets/image/apple1.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 1,
+        img: 'assets/image/apple2.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 2,
+        img: 'assets/image/apple3.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 3,
+        img: 'assets/image/apple4.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 4,
+        img: 'assets/image/apple5.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 5,
+        img: 'assets/image/apple6.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 6,
+        img: 'assets/image/ss1.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 7,
+        img: 'assets/image/ss2.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 8,
+        img: 'assets/image/ss3.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 9,
+        img: 'assets/image/ss4.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 10,
+        img: 'assets/image/ss5.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 11,
+        img: 'assets/image/ss6.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 12,
+        img: 'assets/image/vs1.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 13,
+        img: 'assets/image/vs2.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 14,
+        img: 'assets/image/vs3.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 15,
+        img: 'assets/image/vs4.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 16,
+        img: 'assets/image/vs5.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 17,
+        img: 'assets/image/vs6.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 18,
+        img: 'assets/image/op.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+
+    {
+        id: 29,
+        img: 'assets/image/op2.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 20,
+        img: 'assets/image/op3.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 21,
+        img: 'assets/image/op4.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 22,
+        img: 'assets/image/op5.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+    {
+        id: 23,
+        img: 'assets/image/op6.png',
+        name: 'Điện Thoại iPhone 12 Pro Max 128GB ',
+        price: 29999000,
+        save: 5,
+        delievery: 'Thời gian giao :  3 - 4 ngày ',
+        itemInCart: false
+    },
+
+];
+let cartList = []; //array to store cart lists
+
+var i;
+var detail = document.getElementsByClassName('card-item');
+var detailsImg = document.getElementById('details-img')
+var detailTitle = document.getElementById('detail-title')
+var detailPrice = document.getElementById('detail-price')
+var product_info = document.getElementById('product-info')
+
+var youSave = document.getElementById('you-save');
+var detailsPage = document.getElementById('details-page');
+var back = document.getElementById('buy')
+back.addEventListener('click', refreshPage)
+var addToCarts = document.querySelectorAll('#add-to-cart')
+var cart = document.getElementById('cart');
+
+// click event to display cart page
+
+cart.addEventListener('click', displayCart)
+
+var carts = document.getElementById('carts');
+
+//click events to add items to cart from details page
+var currentCustumer = JSON.parse(localStorage.getItem('listCustumer'));
+// if(localStorage.getItem("listCustumer") != null || localStorage.getItem("listCustumer")===undefined ){
+carts.addEventListener('click', () => addToCart(getId))
+
+// }else{
+//     window.open('dangnhap.html');
+// }
+
+var home = document.getElementById('backhome');
+// var home = document.getElementById('logo');
+
+//click event to hide cart page and return to home page
+home.addEventListener('click', hideCart);
+
+//events on dynamically created element to remove items from list
+document.addEventListener('click', function (e) {
+    if (e.target.id == 'remove') {
+        var itemId = e.target.parentNode.id
+        removeFromCart(itemId)
+    }
+})
+
+
+//click event to display details page
+for (i = 0; i < data.length; i++) {
+    detail[i].addEventListener('click', handleDetail)
+}
+
+var getId;
+// var getTime;
+for (i = 0; i < addToCarts.length; i++) {
+    addToCarts[i].addEventListener('click', function (e) {
+        if (localStorage.getItem("listCustumer") === null) {
+            window.location = 'dangnhap.html';
+        }
+        else {
+            addToCarts.forEach(val => val.addEventListener('click', () => addToCart(val.parentNode.id)));
+        }
+    })
+}
+
+// details function
+function handleDetail(e) {
+    detailsPage.style.display = 'block'
+    getId = this.parentNode.id;
+    // getTime = this.parentNode.delievery;
+
+    detailsImg.src = data[getId].img;
+    detailTitle.innerHTML = data[getId].name;
+    detailPrice.innerHTML = 'Giá tiền :  ' + data[getId].price.toLocaleString('vi', { style: 'currency', currency: 'VND' }) + 'VNĐ';
+    youSave.innerHTML = 'Tiết kiệm : ( ' + data[getId].save + '%)';
+}
+
+// add item to the cart
+var listCustumer = JSON.parse(localStorage.getItem('listCustemer'));
+
+function addToCart(id) {
+    if (!data[id].itemInCart) {
+        cartList = [...cartList, data[id]];
+        addItem()
+        alert('Đã thêm vào giỏ hàng')
+    }
+    // else {
+    //     alert('Sản phẩm đã tồn tại trong giỏ hàng')
+    // }
+    data[id].itemInCart = true
+
+}
+
+//back to main page
+function refreshPage() {
+    detailsPage.style.display = 'none'
+}
+
+// hide your cart page
+function hideCart() {
+    document.getElementById('main').style.display = "block";
+    document.getElementById('sidebar').style.display = "block";
+    document.getElementById('product-info').style.display = "block";
+    document.getElementById('cart-container').style.display = "none";
+}
+
+//display your cart page
+function displayCart() {
+    document.getElementById('main').style.display = "none";
+    document.getElementById('sidebar').style.display = "none";
+    document.getElementById('details-page').style.display = "none";
+    document.getElementById('cart-container').style.display = "block";
+    if (cartList.length == 0) {
+        document.getElementById('cart-with-items').style.display = "none";
+        document.getElementById('empty-cart').style.display = "block";
+        document.getElementById('product-info').style.display = "none";
+
+    }
+    else {
+        document.getElementById('empty-cart').style.display = "none";
+        document.getElementById('cart-with-items').style.display = "block";
+        document.getElementById('product-info').style.display = "none";
+
+
+    }
+}
+
+var totalAmount;
+var totalItems;
+var totalSaving;
+
+//add item to the cart
+function addItem() {
+    totalAmount = 0;
+    totalItems = 0;
+    totalSaving = 0
+    var clrNode = document.getElementById('item-body');
+    clrNode.innerHTML = '';
+    console.log(clrNode.childNodes)
+    cartList.map((cart) => {
+        var cartCont = document.getElementById('item-body');
+        totalAmount = totalAmount + cart.price;
+        totalSaving = totalSaving + cart.save;
+        totalItems = totalItems + 1;
+
+        var tempCart = document.createElement('div')
+        tempCart.setAttribute('class', 'cart-list');
+        tempCart.setAttribute('id', cart.id);
+
+        var listImg = document.createElement('img');
+        listImg.setAttribute('id', 'list-img');
+        listImg.src = cart.img
+        tempCart.appendChild(listImg)
+
+        var listName = document.createElement('h3');
+        listName.setAttribute('class', 'list-name');
+        listName.innerHTML = cart.name;
+        tempCart.appendChild(listName)
+
+        var listPay = document.createElement('h3');
+        listPay.setAttribute('class', 'pay');
+        listPay.innerHTML = cart.price.toLocaleString('vi', { style: 'currency', currency: 'VND' });
+        tempCart.appendChild(listPay);
+
+        var listQuantity = document.createElement('h3');
+        listQuantity.setAttribute('class', 'quantity');
+        listQuantity.innerHTML = '1';
+        tempCart.appendChild(listQuantity);
+
+        var listTrash = document.createElement('i');
+        listTrash.setAttribute('class', 'fas fa-trash-alt');
+        listTrash.setAttribute('id', 'remove');
+        tempCart.appendChild(listTrash);
+
+        cartCont.appendChild(tempCart)
+
+    })
+    totalAmount = totalAmount.toLocaleString('vi', { style: 'currency', currency: 'VND' });//format currency
+    document.getElementById('total-amount').innerHTML = 'Tổng tiền : ' + totalAmount;
+    // document.getElementById('total-items').innerHTML = 'Số lượng : ' + totalItems;
+    // document.getElementById('you-saved').innerHTML = 'Tiết kiệm : ' + totalSaving + 'VNĐ';
+    document.getElementById('total').style.display = "block";
+    document.getElementById('current-quantity').innerHTML = totalItems;
+    localStorage.setItem('shopping cart', JSON.stringify(cartList));
+    localStorage.setItem('product', JSON.stringify(data));
+}
+
+//remove item from the cart
+function removeFromCart(itemId) {
+    data[itemId].itemInCart = false
+    cartList = cartList.filter((list) => list.id != itemId);
+    addItem()
+    if (cartList.length == 0) {
+        document.getElementById('cart-with-items').style.display = "none";
+        document.getElementById('empty-cart').style.display = "block";
+    }
+}
+//////tìm kiém, kepup
+const storeProduct = document.querySelectorAll(".item-container")
+const search = document.getElementById("search");
+search.addEventListener("keyup", (e) => {
+    e.preventDefault();
+    const searchValue = search.value.toLowerCase().trim();
+    for (i = 0; i < storeProduct.length; i++) {
+        if (storeProduct[i].classList.contains(searchValue)) {
+            storeProduct[i].style.display = "block";
+        } else if (searchValue == "") {
+            storeProduct[i].style.display = "block";
+        } else {
+            storeProduct[i].style.display = "none";
+        }
+    }
+})
+
+
+// data-failter sản phẩm
+
+const btns = document.querySelectorAll('.btn');
+const storeProducts = document.querySelectorAll('.item-container');
+// const disableButton = document.getElementById("myBtn");
+
+for (i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', (e) => {
+        e.preventDefault()
+        const filter = e.target.dataset.filter;
+        console.log(filter);
+
+        storeProducts.forEach((product) => {
+            if (filter === 'all') {
+                product.style.display = 'block'
+            } else {
+                if (product.classList.contains(filter)) {
+                    product.style.display = 'block'
+                }
+                else {
+                    product.style.display = 'none'
+                }
+            }
+        });
+    });
+};
+
+//tìm kiếm khi bấm tìm kiếm
+
+function myFunctionTim() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    ul = document.getElementsByClassName("card");
+    li = document.getElementsByClassName("item-container");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("p")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+
+
+}
+//readmore
+function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+//sxep
+function sortListDir() {
+    var list, i, switching, b, shouldSwitch, dir, switchcount = 0;
+    list = document.getElementById("sort");
+    switching = true;
+    dir = "asc";
+    while (switching) {
+        switching = false;
+        b = list.querySelectorAll("#item-name");
+        c = list.querySelectorAll(".item-container");
+        for (i = 0; i < (c.length - 1); i++) {
+            shouldSwitch = false;
+            if (dir == "asc") {
+                if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            c[i].parentNode.insertBefore(c[i + 1], c[i]);
+            switching = true;
+            switchcount++;
+        } else {
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
+//hiện user
+var currenUser = document.getElementById("current-user");
+const currentCustumer1 = JSON.parse(localStorage.getItem('currentCustumer'));
+console.log(currentCustumer1);
+if (currentCustumer1 == null || currentCustumer1.username == '') {
+} else {
+    currenUser.innerHTML = currentCustumer1.username;
+}
